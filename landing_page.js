0e24fb84a19ba1ee975362466c7a21de567d7de8
Seshu© <seshu.m20@iiits.in>
2024-02-10 22:54:55 +0530

    1st commit

---
+++ landing_page.js
@@ -0,0 +1,4 @@
+function toggleNavbar() {
+    var sideNavigationBar = document.getElementById("sideNavigationBar");
+    sideNavigationBar.style.width = sideNavigationBar.style.width === "50%" ? "0%" : "50%";
+}
